adac8f5c4cfc2d7a36d1b944de03c32a4445eff6
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-05-09 07:18:44 -0300

    fix/config prisma and route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
+import { prisma } from "./prisma"
 import express from "express"
 
 const app = express();
 
-app.get('/users',(req,res)=>{
-    return res.send("hello Word")
+// GET,POST,PUT,PATCH,DELETE
+
+//GET = Buscar informação
+// POST = Cadastrar informação
+// PUT = Atualizar informação de um entidade
+// PATCH = Atualizar uma informação unica de uma entidade
+// DELETE = Deletar uma infomação
+app.use(express.json());
+
+app.post('/feedbacks', async (req,res)=>{
+
+    const {type,comment,screenshot} = req.body;
+
+   const feedback = await prisma.feddback.create({
+       data:{
+           type,
+           comment,
+           screenshot,
+       } 
+    })
+
+
+    return res.status(201).json({data:feedback});
 })
 
 app.listen(3333,()=>{
