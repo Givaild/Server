cd76c190a470be9597e080ed559d42443749c04d
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-05-09 09:11:57 -0300

    fix/config send email

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { prisma } from "./prisma"
 import express from "express"
+import nodemailer from "nodemailer"
 
 const app = express();
 
@@ -9,8 +10,17 @@ const app = express();
 // POST = Cadastrar informação
 // PUT = Atualizar informação de um entidade
 // PATCH = Atualizar uma informação unica de uma entidade
-// DELETE = Deletar uma infomação
+// DELETE = Deletar uma informação
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "78faac51a36a7e",
+      pass: "33a9fd7bda1463"
+    }
+  });
 
 app.post('/feedbacks', async (req,res)=>{
 
@@ -23,6 +33,22 @@ app.post('/feedbacks', async (req,res)=>{
            screenshot,
        } 
     })
+
+    transport.sendMail({
+
+        from: "Equipe Feedget <oi@feedget.com>",
+        to: "Givaildo Andrade <gil.nmy@gmail.com>",
+        subject: "novo Feedback",
+        html:[
+            ` <div style=font-family:sans-serif; font-size:16px; color: #111;>`,
+            `<p> Tipo de Feddback: ${type}<p>`,
+            `<p> Comentário: ${comment}<p>`,
+            `</div>`
+
+        ].join("\n")
+
+
+    });
 
 
     return res.status(201).json({data:feedback});
